88302a52612290a6bc4a527d3f9396db16b51158
Jeffrey Paul <jp@eeqj.com>
2018-07-12 00:15:20 -0400

    formatting

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
-import * as firebase from 'firebase';
+import * as firebase from "firebase";
 
 const config = {
-  apiKey: et.config.FIREBASE_API_KEY,
-  authDomain: et.config.FIREBASE_AUTH_DOMAIN,
-  databaseURL: et.config.FIREBASE_DATABASE_URL,
-  projectId: et.config.FIREBASE_PROJECT_ID,
-  storageBucket: et.config.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: et.config.FIREBASE_MESSAGING_SENDER_ID,
+    apiKey: et.config.FIREBASE_API_KEY,
+    authDomain: et.config.FIREBASE_AUTH_DOMAIN,
+    databaseURL: et.config.FIREBASE_DATABASE_URL,
+    projectId: et.config.FIREBASE_PROJECT_ID,
+    storageBucket: et.config.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: et.config.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
