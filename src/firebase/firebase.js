2e8a58fcbd9c756e00359d170e0cf04d90e384e8
Ken Jannette <KenJannette@Kens-MBP.localdomain>
2018-07-12 01:07:37 -0400

    revert to 88302a52612290a6bc4a527d3f9396db16b51158

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,17 +1,13 @@
 import * as firebase from "firebase";
 
-var config = {}
-
-if(window && typeof et !== 'undefined') {
-    config = {
-        apiKey: et.config.FIREBASE_API_KEY,
-        authDomain: et.config.FIREBASE_AUTH_DOMAIN,
-        databaseURL: et.config.FIREBASE_DATABASE_URL,
-        projectId: et.config.FIREBASE_PROJECT_ID,
-        storageBucket: et.config.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: et.config.FIREBASE_MESSAGING_SENDER_ID
-    };
-}
+const config = {
+    apiKey: et.config.FIREBASE_API_KEY,
+    authDomain: et.config.FIREBASE_AUTH_DOMAIN,
+    databaseURL: et.config.FIREBASE_DATABASE_URL,
+    projectId: et.config.FIREBASE_PROJECT_ID,
+    storageBucket: et.config.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: et.config.FIREBASE_MESSAGING_SENDER_ID
+};
 
 firebase.initializeApp(config);
 
