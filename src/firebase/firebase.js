5b6f8109ce426279cb568aeb5bfd926a59ac49d9
Ken Jannette <KenJannette@Kens-MBP.localdomain>
2018-07-12 00:57:13 -0400

    only try to set firebase config in browser or not in test

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,17 @@
 import * as firebase from "firebase";
 
-const config = {
-    apiKey: et.config.FIREBASE_API_KEY,
-    authDomain: et.config.FIREBASE_AUTH_DOMAIN,
-    databaseURL: et.config.FIREBASE_DATABASE_URL,
-    projectId: et.config.FIREBASE_PROJECT_ID,
-    storageBucket: et.config.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: et.config.FIREBASE_MESSAGING_SENDER_ID
-};
+var config = {}
+
+if(window || process.env.NODE_ENV != 'test') {
+    config = {
+        apiKey: et.config.FIREBASE_API_KEY,
+        authDomain: et.config.FIREBASE_AUTH_DOMAIN,
+        databaseURL: et.config.FIREBASE_DATABASE_URL,
+        projectId: et.config.FIREBASE_PROJECT_ID,
+        storageBucket: et.config.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: et.config.FIREBASE_MESSAGING_SENDER_ID
+    };
+}
 
 firebase.initializeApp(config);
 
